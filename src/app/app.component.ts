9709149ec3d372bb1b9f416c7b4e0b9248839197
kinababacarsy <fatoukinediakite717@gmail.com>
2024-12-26 00:51:14 +0000

    démarrage interface assignation-carte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AssignationCarteComponent } from './assignation-carte/assignation-carte.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AssignationCarteComponent],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, NavbarComponent],
+  template: `    <app-navbar></app-navbar>
+                <router-outlet></router-outlet>`
 
 })
 export class AppComponent {
   title = 'SunuPointage';
 }
-
 
 
